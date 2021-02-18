01ed3cc5045d61d4dc1f1da52f4454b60884bb44
hemant sahu <hemantjava90@gmail.com>
2021-02-18 23:09:37 +0530

    Initial commit

---
+++ src/main/java/com/example/springdatamongodb/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.example.springdatamongodb.controller;
+
+import com.example.springdatamongodb.dacument.User;
+import com.example.springdatamongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/list")
+    public ResponseEntity<List<User>> getUsers() {
+        final List<User> userList = userRepository.findAll();
+        return ResponseEntity.ok(userList);
+    }
+    @PostMapping("/create")
+    public ResponseEntity<User> saveUser(@RequestBody User user){
+        final User save = userRepository.save(user);
+     return ResponseEntity.ok(save);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<User> findUser(@PathVariable("id") Integer userId){
+        final Optional<User> user = userRepository.findById(userId);
+        if (user.isPresent())
+            return ResponseEntity.ok(user.get());
+        return ResponseEntity.notFound().build();
+    }
+    @PutMapping("/update")
+    public ResponseEntity<User> updateUser(@RequestBody User user){
+        final Optional<User> userOptional = userRepository.findById(user.getId());
+        if (userOptional.isPresent()){
+            final User userExist = userOptional.get();
+            userExist.setName(user.getName());
+            userExist.setTeamName(user.getTeamName());
+            userExist.setSalary(user.getSalary());
+            final User UpdatedUser = userRepository.save(userExist);
+            return ResponseEntity.ok(UpdatedUser);
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
---
+++ src/main/java/com/example/springdatamongodb/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.springdatamongodb.repository;
+
+import com.example.springdatamongodb.dacument.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public interface UserRepository extends MongoRepository<User,Integer> {
+}
