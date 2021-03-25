64b7a3cb1795fe7d01c006c4c1eea13ec28bb323
hemsahu1 <hemant>
2021-03-25 23:19:30 +0530

    Updated Repository

--- src/main/java/com/example/springdatamongodb/repository/UserRepository.java
+++ src/main/java/com/example/springdatamongodb/repository/UserRepository.java
@@ -2,8 +2,9 @@ package com.example.springdatamongodb.repository;
 
 import com.example.springdatamongodb.dacument.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Repository
 public interface UserRepository extends MongoRepository<User,Integer> {
 }
